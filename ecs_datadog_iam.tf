34f150af94bbc68cc7265d19257cdd43ee438263
Tade <phan.minh.trung@moneyforward.vn>
2020-08-05 16:56:47 +0700

    fix

--- ecs_datadog_iam.tf
+++ ecs_datadog_iam.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "ecs-datadog-role" {
-  name = "${var.env}-${var.identifier}-jenkins-ecs-datadog-role"
+  name               = "${var.env}-${var.identifier}-ecs-datadog-role"
   assume_role_policy = data.aws_iam_policy_document.ecs-task-role-assume.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs-datadog" {
-  role = aws_iam_role.ecs-datadog-role.name
+  role       = aws_iam_role.ecs-datadog-role.name
   policy_arn = aws_iam_policy.ecs-datadog-role-policy.arn
 }
 
 resource "aws_iam_policy" "ecs-datadog-role-policy" {
-  name = "${var.env}-${var.identifier}-datadog-agent-ecs-iam-role-policy"
+  name   = "${var.env}-${var.identifier}-datadog-agent-ecs-iam-role-policy"
   policy = data.aws_iam_policy_document.ecs-datadog-role.json
 }
 
 data "aws_iam_policy_document" "ecs-datadog-role" {
 
   statement {
-    sid = "AllowDatadogToReadECSMetrics"
+    sid    = "AllowDatadogToReadECSMetrics"
     effect = "Allow"
     actions = [
       "ecs:RegisterContainerInstance",
@@ -27,18 +27,18 @@ data "aws_iam_policy_document" "ecs-datadog-role" {
       "ecs:StartTask",
       "ecs:StartTelemetrySession"
     ]
-    resources = [ "*" ]
+    resources = ["*"]
   }
 
 }
 
 data "aws_iam_policy_document" "ecs-task-role-assume" {
   statement {
-    effect = "Allow"
-    actions = [ "sts:AssumeRole" ]
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
     principals {
-      type = "Service"
-      identifiers = [ "ecs-tasks.amazonaws.com" ]
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 }
