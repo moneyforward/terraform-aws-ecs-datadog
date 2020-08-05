09744a8a8aede242199d4801a329ba0eab8ccd6a
Tade <phan.minh.trung@moneyforward.vn>
2020-08-05 15:26:11 +0700

    specific name of datadog iam role, policy

--- aws_datadog_iam.tf
+++ aws_datadog_iam.tf
@@ -3,11 +3,11 @@ data "aws_iam_policy_document" "datadog_aws_integration_assume_role" {
     actions = ["sts:AssumeRole"]
 
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = ["arn:aws:iam::464622532012:root"] # Datadog AWS Account ID
     }
     condition {
-      test = "StringEquals"
+      test     = "StringEquals"
       variable = "sts:ExternalId"
 
       values = [
@@ -20,89 +20,89 @@ data "aws_iam_policy_document" "datadog_aws_integration_assume_role" {
 data "aws_iam_policy_document" "datadog_aws_integration" {
   statement {
     actions = [
-        "apigateway:GET",
-        "autoscaling:Describe*",
-        "budgets:ViewBudget",
-        "cloudfront:GetDistributionConfig",
-        "cloudfront:ListDistributions",
-        "cloudtrail:DescribeTrails",
-        "cloudtrail:GetTrailStatus",
-        "cloudwatch:Describe*",
-        "cloudwatch:Get*",
-        "cloudwatch:List*",
-        "codedeploy:List*",
-        "codedeploy:BatchGet*",
-        "directconnect:Describe*",
-        "dynamodb:List*",
-        "dynamodb:Describe*",
-        "ec2:Describe*",
-        "ecs:Describe*",
-        "ecs:List*",
-        "elasticache:Describe*",
-        "elasticache:List*",
-        "elasticfilesystem:DescribeFileSystems",
-        "elasticfilesystem:DescribeTags",
-        "elasticloadbalancing:Describe*",
-        "elasticmapreduce:List*",
-        "elasticmapreduce:Describe*",
-        "es:ListTags",
-        "es:ListDomainNames",
-        "es:DescribeElasticsearchDomains",
-        "health:DescribeEvents",
-        "health:DescribeEventDetails",
-        "health:DescribeAffectedEntities",
-        "kinesis:List*",
-        "kinesis:Describe*",
-        "lambda:AddPermission",
-        "lambda:GetPolicy",
-        "lambda:List*",
-        "lambda:RemovePermission",
-        "logs:Get*",
-        "logs:Describe*",
-        "logs:FilterLogEvents",
-        "logs:TestMetricFilter",
-        "logs:PutSubscriptionFilter",
-        "logs:DeleteSubscriptionFilter",
-        "logs:DescribeSubscriptionFilters",
-        "rds:Describe*",
-        "rds:List*",
-        "redshift:DescribeClusters",
-        "redshift:DescribeLoggingStatus",
-        "route53:List*",
-        "s3:GetBucketLogging",
-        "s3:GetBucketLocation",
-        "s3:GetBucketNotification",
-        "s3:GetBucketTagging",
-        "s3:ListAllMyBuckets",
-        "s3:PutBucketNotification",
-        "ses:Get*",
-        "sns:List*",
-        "sns:Publish",
-        "sqs:ListQueues",
-        "support:*",
-        "tag:GetResources",
-        "tag:GetTagKeys",
-        "tag:GetTagValues",
-        "xray:BatchGetTraces",
-        "xray:GetTraceSummaries"
+      "apigateway:GET",
+      "autoscaling:Describe*",
+      "budgets:ViewBudget",
+      "cloudfront:GetDistributionConfig",
+      "cloudfront:ListDistributions",
+      "cloudtrail:DescribeTrails",
+      "cloudtrail:GetTrailStatus",
+      "cloudwatch:Describe*",
+      "cloudwatch:Get*",
+      "cloudwatch:List*",
+      "codedeploy:List*",
+      "codedeploy:BatchGet*",
+      "directconnect:Describe*",
+      "dynamodb:List*",
+      "dynamodb:Describe*",
+      "ec2:Describe*",
+      "ecs:Describe*",
+      "ecs:List*",
+      "elasticache:Describe*",
+      "elasticache:List*",
+      "elasticfilesystem:DescribeFileSystems",
+      "elasticfilesystem:DescribeTags",
+      "elasticloadbalancing:Describe*",
+      "elasticmapreduce:List*",
+      "elasticmapreduce:Describe*",
+      "es:ListTags",
+      "es:ListDomainNames",
+      "es:DescribeElasticsearchDomains",
+      "health:DescribeEvents",
+      "health:DescribeEventDetails",
+      "health:DescribeAffectedEntities",
+      "kinesis:List*",
+      "kinesis:Describe*",
+      "lambda:AddPermission",
+      "lambda:GetPolicy",
+      "lambda:List*",
+      "lambda:RemovePermission",
+      "logs:Get*",
+      "logs:Describe*",
+      "logs:FilterLogEvents",
+      "logs:TestMetricFilter",
+      "logs:PutSubscriptionFilter",
+      "logs:DeleteSubscriptionFilter",
+      "logs:DescribeSubscriptionFilters",
+      "rds:Describe*",
+      "rds:List*",
+      "redshift:DescribeClusters",
+      "redshift:DescribeLoggingStatus",
+      "route53:List*",
+      "s3:GetBucketLogging",
+      "s3:GetBucketLocation",
+      "s3:GetBucketNotification",
+      "s3:GetBucketTagging",
+      "s3:ListAllMyBuckets",
+      "s3:PutBucketNotification",
+      "ses:Get*",
+      "sns:List*",
+      "sns:Publish",
+      "sqs:ListQueues",
+      "support:*",
+      "tag:GetResources",
+      "tag:GetTagKeys",
+      "tag:GetTagValues",
+      "xray:BatchGetTraces",
+      "xray:GetTraceSummaries"
     ]
-    effect = "Allow"
+    effect    = "Allow"
     resources = ["*"]
   }
 }
 
 resource "aws_iam_policy" "datadog_aws_integration" {
-  name = "DatadogAWSIntegrationPolicy"
+  name   = "DatadogAWSIntegrationPolicy-${var.env}-${var.identifier}"
   policy = data.aws_iam_policy_document.datadog_aws_integration.json
 }
 
 resource "aws_iam_role" "datadog_aws_integration" {
-  name = "DatadogAWSIntegrationRole"
-  description = "Role for Datadog AWS Integration"
+  name               = "DatadogAWSIntegrationRole-${var.env}-${var.identifier}"
+  description        = "Role for Datadog AWS Integration"
   assume_role_policy = data.aws_iam_policy_document.datadog_aws_integration_assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "datadog_aws_integration" {
-  role = aws_iam_role.datadog_aws_integration.name
+  role       = aws_iam_role.datadog_aws_integration.name
   policy_arn = aws_iam_policy.datadog_aws_integration.arn
 }
